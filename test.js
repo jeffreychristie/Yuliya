ca8b36311b07d91dbdc6eab7d22e0090ae897303
jeffreychristie <43511412+jeffreychristie@users.noreply.github.com>
2018-09-23 03:04:37 -0500

    Add files via upload

---
+++ test.js
@@ -0,0 +1,26 @@
+const assert = require('assert');
+const warmup = require('./src/index.js');
+
+console.log("Starting it up...");
+
+describe('warmup', () => {
+  it('warm cold', () => {
+    const fahrenheit = warmup(-20);
+    assert.deepEqual(fahrenheit, -4);
+  });
+
+  it('warm cool', () => {
+    const fahrenheit = warmup(0);
+    assert.deepEqual(fahrenheit, 32);
+  });
+
+  it('warm medium', () => {
+    const fahrenheit = warmup(15);
+    assert.deepEqual(fahrenheit, 59);
+  });
+
+  it('warm hot', () => {
+    const fahrenheit = warmup(40);
+    assert.deepEqual(fahrenheit, 104);
+  });
+});
